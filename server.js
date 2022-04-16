e40ff39b9262fd6be2c6e639cca71b9c651d572f
Ermiyas  Hailemichael <yemeerma11@gmail.com>
2022-04-16 14:52:28 -0700

    setting up database

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const res = require('express/lib/response');
 
 const app = express();
 
 const port = 3000
 
+
+
+const drinks = require('./models/drinks.js');
+
 app.get("/",(req, res)=>{
 
     res.send("Welcome to the Gitpub App!")
 })
+
+app.get("/drinks",(req, res)=>{
+
+    res.send(drinks)
+})
+
 
 app.listen(port,() => {
     console.log("Listening port 3000!")

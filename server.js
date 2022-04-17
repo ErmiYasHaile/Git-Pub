a81fde5288b3df9c30aaa871e08c5d0992727db6
Ermiyas  Hailemichael <yemeerma11@gmail.com>
2022-04-16 18:57:35 -0700

    food update

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express');
+const req = require('express/lib/request');
 const res = require('express/lib/response');
 const app = express();
 const port = 3000
 
 const drinks = require('./models/drinks.js');
 
+const foods = require ('./models/food.js');
+
 app.get("/",(req, res)=>{
 
     res.send("Welcome to the Gitpub App!")
 })
 
-
+app.get("/foods",(req, res) => {
+    res.render('food_index.ejs',{foods})
+})
 
 app.get("/drinks",(req, res)=>{
 // i use the varialbe drinks on my index.ejs file
@@ -23,6 +28,10 @@ app.get("/drinks/:id",(req, res)=>{
     // mydrink is a file holder like the name, price and image
     res.render('drinks_show.ejs',{mydrink: drinks[req.params.id]})
     })
+
+app.get("/foods/:id",(req,res)=>{
+    res.render('food_show.ejs',{menu: foods[req.params.id]})
+})
 
 app.listen(port,() => {
     console.log("Listening port 3000!")
